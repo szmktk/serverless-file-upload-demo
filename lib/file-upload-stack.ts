ace13bcf8cc3a6dfa3f3c58dacd39f73f98ce793
Szymon Mikitiuk <szymon.mikitiuk@merapar.com>
2022-03-13 20:43:33 +0100

    Add cloud resources

--- lib/file-upload-stack.ts
+++ lib/file-upload-stack.ts
@@ -1,19 +1,73 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apigwv2 from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { CfnOutput, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
+import * as path from 'path';
+
 
 export class FileUploadStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'FileUploadQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const uploadBucket = new s3.Bucket(this, `${id}-uploadBucket`, {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      cors: [{
+        allowedHeaders: ['*'],  // insecure, for illustrative purposes only
+        allowedMethods: [
+          s3.HttpMethods.GET,
+          s3.HttpMethods.PUT,
+          s3.HttpMethods.HEAD,
+        ],
+        allowedOrigins: ['*'],  // insecure, for illustrative purposes only
+      }]
+    })
+
+    const getSignedUrlFunction = new NodejsFunction(this, `${id}-getSignedUrlFunction`, {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'handler',
+      memorySize: 128,
+      timeout: Duration.seconds(5),
+      entry: path.join(__dirname, '../lambda/getSignedUrl.ts'),
+      environment: {
+        URL_EXPIRATION_SECONDS: '300',
+        UPLOAD_BUCKET_NAME: uploadBucket.bucketName
+      }
     });
 
-    const topic = new sns.Topic(this, 'FileUploadTopic');
+    getSignedUrlFunction.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['s3:putObject'],
+        resources: [`${uploadBucket.bucketArn}/*`]
+      })
+    )
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const httpApi = new apigwv2.HttpApi(this, `${id}-httpApi`, {
+      description: 'ApiGateway API that invokes a function responsible for creating a pre-signed S3 url',
+      corsPreflight: {
+        allowHeaders: ['*'],  // insecure, for illustrative purposes only
+        allowMethods: [
+          apigwv2.CorsHttpMethod.GET,
+          apigwv2.CorsHttpMethod.PUT,
+          apigwv2.CorsHttpMethod.HEAD,
+        ],
+        allowOrigins:['*'],  // insecure, for illustrative purposes only
+      },
+    })
+
+    httpApi.addRoutes({
+      path: '/',
+      methods: [apigwv2.HttpMethod.GET],
+      integration: new HttpLambdaIntegration(id, getSignedUrlFunction),
+    })
+
+    new CfnOutput(this, id, {
+      value: httpApi.url!,
+    });
   }
 }
